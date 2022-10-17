92815ee30111b09a9a1e20a8c6093f4940fc7211
mdwisu <dwisusanto784@gmail.com>
2022-10-17 15:00:30 +0700

    selesai fitur login

--- js/controllers/Auth.js
+++ js/controllers/Auth.js
@@ -1,20 +1,22 @@
-export const Login = async(username, password) => {
-    try {
-        // get data
-        const URL = 'https://6348abae0b382d796c75343e.mockapi.io/users';
-        let response = await fetch(URL);
-        let result = await response.json();
-        console.log(result);
-        // find user
-        let findUser = result.find(item => item.username == username)
-        console.log(findUser);
-        if (!findUser) {
-            return 'User tidak ditemukan'
-        } else if (findUser.password != password) {
-            return 'Password anda salah'
-        }
-      } catch (error) {
-        console.log(error);
-        return null;
-      }
-}
+export const Login = async (username, password) => {
+  try {
+    // get data
+    const URL = 'https://6348abae0b382d796c75343e.mockapi.io/users';
+    let response = await fetch(URL);
+    let result = await response.json();
+    // find user
+    let findUser = result.find((item) => item.username == username);
+    // if user not found, if wrong pass, else login
+    if (!findUser) {
+      return 'User tidak ditemukan';
+    } else if (findUser.password != password) {
+      return 'Password anda salah';
+    } else {
+      sessionStorage.setItem('login', true);
+      location.href = './dashboard.html';
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
-import {Login} from './controllers/Auth.js'
+import { Login } from './controllers/Auth.js';
 
-Login("dwi2@gmail.com", "dwi")
+// ! Login
+const formLogin = document.getElementById('form-login');
+
+formLogin.onsubmit = (e) => {
+  e.preventDefault();
+  // menghapus alert login supaya tidak menumpuk
+  const alertLogin = document.getElementById('alert-login')
+  if (alertLogin) {
+    alertLogin.remove()
+  }
+  // get input form
+  const inputEmail = document.getElementById('inputEmail').value
+  const inputPassword = document.getElementById('inputPassword').value
+  // login
+  Login(inputEmail, inputPassword).then(
+    (result) => {
+      console.log(result);
+      // Create a element alert before form
+      const div = document.createElement('div');
+      div.id = 'alert-login'
+      div.className += 'alert';
+      div.className += ' alert-danger';
+      div.innerHTML = `${result}`;
+      formLogin.before(div);
+    }
+  );
+};
