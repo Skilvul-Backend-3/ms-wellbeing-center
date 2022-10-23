c0edd0ff23dcfd5589a9cff4aaaeb548ed888c9d
xyloph0xy <sofiyahluluk7@gmail.com>
2022-10-23 20:30:06 +0700

    push kategori

--- js/controllers/videos.js
+++ js/controllers/videos.js
@@ -19,3 +19,15 @@ export const getVideoByVideoId = async (videoId) => {
     return error;
   }
 };
+
+export const getCategory = async (kategori) => {
+  try {
+    const URL = `https://6348abae0b382d796c75343e.mockapi.io/videos?category=${kategori}`;
+    let response = await fetch(URL);
+    let result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
