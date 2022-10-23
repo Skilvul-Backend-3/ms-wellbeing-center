fdc11673b706a57b8a24750fd4a6857ac0a3de85
mdwisu <dwisusanto784@gmail.com>
2022-10-23 20:09:54 +0700

    menambahkan fitur search

--- js/controllers/videos.js
+++ js/controllers/videos.js
@@ -13,9 +13,46 @@ export const getVideos = async () => {
 export const getVideoByVideoId = async (videoId) => {
   try {
     const dataVideo = await getVideos();
-    let findVideo = dataVideo.find((item) => item.videoId == videoId)
+    let findVideo = dataVideo.find((item) => item.videoId == videoId);
     return findVideo;
   } catch (error) {
     return error;
+  }
+};
+
+export const createCard = (dataVideo) => {
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = '';
+  // create card youtube
+  dataVideo.map((item) => {
+    cardContainer.innerHTML += `<div id="${item.videoId}" class="card m-3 hoveryt" style="width: 14rem">
+    <img src="https://img.youtube.com/vi/${item.videoId}/mqdefault.jpg">
+    <div class="card-body">
+      <h6 class="card-title">${item.title}</h6>
+    </div>
+  </div>`;
+  });
+
+  const allCard = document.getElementsByClassName('card');
+
+  for (const item of allCard) {
+    item.addEventListener('click', (event) => {
+      location.href = `./detail-video.html?videoId=${event.currentTarget.id}`;
+    });
+  }
+};
+
+export const getVideoBySearch = async (search) => {
+  try {
+    const URL = 'https://6348abae0b382d796c75343e.mockapi.io/videos';
+    let response = await fetch(URL);
+    let result = await response.json();
+    result = result.filter((item) =>
+      item.title.toLowerCase().includes(search.toLowerCase())
+    );
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,14 @@
 import { Me } from './controllers/Auth.js';
-import { getVideos } from './controllers/videos.js';
+import {
+  createCard,
+  getVideoBySearch,
+  getVideos,
+} from './controllers/videos.js';
 import { verifyUser } from './middleware/authUser.js';
 
 // verify user
-if (verifyUser()) { //panggil function verify user
+if (verifyUser()) {
+  //panggil function verify user
   location.href = './login.html';
   alert(verifyUser());
 }
@@ -14,34 +19,26 @@ let data = async () => {
   return result;
 };
 let my = await data();
-console.log(my.data);
 
 const welcome = document.getElementById('welcome');
 welcome.innerText = `Welcome ${my.data.fullname}`;
 
 // get all data video
 let dataVideo = await getVideos();
-function clickCard(id) {
-  console.log(id);
-}
+console.log(dataVideo);
+createCard(dataVideo);
 
-const cardContainer = document.getElementById('card-container');
-dataVideo.map((item) => {
-  cardContainer.innerHTML += `<div id="${item.videoId}" class="card m-3 hoveryt" style="width: 14rem">
-  <img src="https://img.youtube.com/vi/${item.videoId}/mqdefault.jpg">
-  <div class="card-body">
-    <h6 class="card-title">${item.title}</h6>
-  </div>
-</div>`;
-  console.log(item);
+
+const formSearch = document.getElementsByTagName('form')[0];
+formSearch.addEventListener('keyup', async (e) => {
+  e.preventDefault();
+  let searchTerm = document.getElementById('search').value;
+  console.log(searchTerm);
+  if (!searchTerm) {
+    dataVideo = await getVideos();
+    createCard(dataVideo);
+  } else {
+    dataVideo = await getVideoBySearch(searchTerm);
+    createCard(dataVideo)
+  }
 });
-
-const allCard = document.getElementsByClassName('card');
-console.log(allCard);
-
-for (const item of allCard) {
-  console.log(item);
-  item.addEventListener('click', (event) => {
-    location.href = `./detail-video.html?videoId=${event.currentTarget.id}`;
-  });
-}
